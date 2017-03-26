aba6f1791e42204211c27ac34cace98f8ac31f07
Dilya <fooont@bk.ru>
2017-03-26 14:26:21 +0300

    'classwork2'

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var googlecdn = require('gulp-google-cdn');
+var clean = require('gulp-clean');
+ 
+gulp.task('default', function () {
+    return gulp.src('index.html')
+        .pipe(googlecdn(require('./bower.json')))
+        .pipe(gulp.dest('./dist'));
+});
+
+
+//очищение папки 
+gulp.task('clean', function (done) {
+    return gulp.src('./dist', {read: false})
+        .pipe(clean());
+    done();
+}); 
+
+gulp.task('scss', gulp.series('clean', function (done) {
+    return gulp.src('./scss/**/*.scss')
+    .pipe(sass({
+      outputStyle: 'nested'
+  }).on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css'));
+    done();
+}));
+
+
+
+
+gulp.task('scss:watch', function () {
+  gulp.watch('./scss/**/*.scss', gulp.series('clean','scss'));
+});
+
+
